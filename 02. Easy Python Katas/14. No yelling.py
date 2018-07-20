37cd15bcc49b9dd5f24f7a5c1681b9861785e9dd
andriihrypa <hrypaas@gmail.com>
2018-07-20 23:01:17 +0300

    commit

--- 02. Easy Python Katas/14. No yelling.py
+++ 02. Easy Python Katas/14. No yelling.py
@@ -1,12 +1,9 @@
-# https://www.codewars.com/kata/no-yelling/train/python
-
-
-def filter_words(st):
-    st = st.lower()
-    st = st.split()
-    st = " ".join(st)
-    st = st.capitalize()
-    return st
+def filter_words(yelling):
+    no_yelling = yelling.lower()
+    no_yelling = no_yelling.split()
+    no_yelling = " ".join(no_yelling)
+    no_yelling = no_yelling.capitalize()
+    return no_yelling
 
 
 print(filter_words(" no   YELLING!!!"))
