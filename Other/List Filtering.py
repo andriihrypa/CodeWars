2b808652fc234c3ff8a12cc1153d1722e196bd03
andriihrypa <hrypaas@gmail.com>
2018-07-20 23:14:39 +0300

    File List Filtering.py was modified

--- Other/List Filtering.py
+++ Other/List Filtering.py
@@ -1,18 +1,18 @@
 # Solution using Loop
 
 
-# def filter_list(l):
-#     ints = []
-#     for i in l:
-#         if type(i) is int:
-#             ints.append(i)
-#     return ints
+def filter_list(l):
+    ints = []
+    for i in l:
+        if type(i) is int:
+            ints.append(i)
+    return ints
 
 # Solution using List comprehension
 
 
 def filter_list(l):
-	return [i for i in l if type(i) is int]
+    return [elem for elem in l if type(elem) is int]
 
 
-print(filter_list([1,2,'a','b']))
+print(filter_list([1, 2, 'a', 'b']))
