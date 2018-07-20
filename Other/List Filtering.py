0af005ff2b82f299d404c7a47b3338ea5f492dec
andriihrypa <hrypaas@gmail.com>
2018-07-20 23:08:55 +0300

    File List Filtering.py was added

--- Other/List Filtering.py
+++ Other/List Filtering.py
@@ -1,12 +1,18 @@
-# https://www.codewars.com/kata/list-filtering/train/python
+# Solution using Loop
+
+
+# def filter_list(l):
+#     ints = []
+#     for i in l:
+#         if type(i) is int:
+#             ints.append(i)
+#     return ints
+
+# Solution using List comprehension
 
 
 def filter_list(l):
-    ints = []
-    for i in l:
-        if type(i) is int:
-            ints.append(i)
-    return ints
+	return [i for i in l if type(i) is int]
 
 
 print(filter_list([1,2,'a','b']))
