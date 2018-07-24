c6d15139be01868e4f602c81c7f0a7468b3935b2
andriihrypa <hrypaas@gmail.com>
2018-07-24 18:47:13 +0300

    updated file Dictionary of Factors

--- Other/Dictionary of Factors.py
+++ Other/Dictionary of Factors.py
@@ -1,14 +1,13 @@
 def factorsRange(n, m):
     factors = {}
-    temp = []
     for i in range(n, m+1):
+        current_list_of_factors = []
         for j in range(2, i):
             if i % j == 0:
-                temp.append(j)
-        if not temp:
-            temp = ["None"]
-        factors[i] = temp
-        temp = []
+                current_list_of_factors.append(j)
+        if not current_list_of_factors:
+            current_list_of_factors = ["None"]
+        factors[i] = current_list_of_factors
     return factors
 
 
