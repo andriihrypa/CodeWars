f2a943e7d568409bfdc4ba2c63117b4445a4fc74
andriihrypa <hrypaas@gmail.com>
2018-07-31 16:14:09 +0300

    Longest polindrome was added

--- Other/Longest Palindrome.py
+++ Other/Longest Palindrome.py
@@ -1,1 +1,11 @@
-def longest_palindrome (s):
+def longest_palindrome(s):
+    slice_lengths = []
+    for i in range(len(s)):
+        for j in range(len(s), i, -1):
+            slice = s[i:j]
+            if slice == slice[::-1]:
+                slice_lengths.append(len(slice))
+    return max(slice_lengths) if len(slice_lengths) > 0 else 0
+
+
+print(longest_palindrome("baablkj12345432133d"))
